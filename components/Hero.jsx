c8f19eb3297bf5fd1de6a935e24e8834007eba6b
Hussnain Ali <hussnain.stack@gmail.com>
2026-08-20 19:22:27 +0500

    image hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -125,7 +125,7 @@ export default function Hero({ onNavigate }) {
 
               {/* Hero Laptop Image */}
               <Image
-                src="/images/hero-sideImage.jpg"
+                src="/images/hero-Hero-sideImage.jpg"
                 alt="Modern business website design"
                 width={1200}
                 height={800}
